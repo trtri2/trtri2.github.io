05840182718a63b64feb893be695f8c2171f0d63
trtri2 <39638939+trtri2@users.noreply.github.com>
2019-07-19 12:34:35 -0700

    Update bracket.js for 8player
    +8player round robin feature

--- bracket.js
+++ bracket.js
@@ -4,22 +4,39 @@ var player3 = new Object();
 var player4 = new Object();
 var player5 = new Object();
 var player6 = new Object();
+var player7 = new Object();
+var player8 = new Object();
 var playerArray = [player1, player2, player3, player4, player5, player6];
+var totalWins = 0;
+var numOfGames = 0;
+var numOfPlayers = 0;
 var drawBasket = [];
 
 
 //Takes the 6 player inputs and generates them in an array. This populates the bracket and scoreboard.
 //TODO: add an extra 'are you sure?' prompt when generating to avoid accidental overwriting. Or disable/remove the button.
-function generate(){
+function generate(eightP){
   //assign names to player objects
-  for (i = 0; i<playerArray.length; i++){
+
+
+  if (eightP){
+    numOfPlayers = 8;
+    numOfGames = 7;
+    playerArray.push(player7);
+    playerArray.push(player8);
+  } else {
+    numOfPlayers = 6;
+    numOfGames = 6;
+  }
+
+  for (i = 0; i<numOfPlayers; i++){
     playerArray[i].name = document.getElementById("p"+(i+1)).value;
     playerArray[i].wins = 0;
     playerArray[i].losses = 0;
   }
 
   //randomize the players
-  randomize(playerArray);
+//  randomize(playerArray);
 
   //Initialize scoreboard
   for (i = 0; i<playerArray.length; i++){
@@ -30,19 +47,34 @@ function generate(){
     var tpscore = "tp"+(i+1)+"score";
     var tpgames = "tp"+(i+1)+"games";
     document.getElementById(tpscore).innerHTML = playerArray[i].wins+"-"+playerArray[i].losses;
-    document.getElementById(tpgames).innerHTML = 6 - (playerArray[i].wins+playerArray[i].losses);
+    document.getElementById(tpgames).innerHTML = numOfGames - (playerArray[i].wins+playerArray[i].losses);
     document.getElementById("entry").style.display = "none";
   }
 
   //Populate bracket after the players are randomized.
-  for (j = 0; j<6; j++){
-  document.getElementsByClassName("sd1")[j].innerHTML = playerArray[0].name;
-  document.getElementsByClassName("sd2")[j].innerHTML = playerArray[1].name;
-  document.getElementsByClassName("sd3")[j].innerHTML = playerArray[2].name;
-  document.getElementsByClassName("sd4")[j].innerHTML = playerArray[3].name;
-  document.getElementsByClassName("sd5")[j].innerHTML = playerArray[4].name;
-  document.getElementsByClassName("sd6")[j].innerHTML = playerArray[5].name;
+  if (eightP){
+    for (j = 0; j<7; j++){
+    document.getElementsByClassName("sd1")[j].innerHTML = playerArray[0].name;
+    document.getElementsByClassName("sd2")[j].innerHTML = playerArray[1].name;
+    document.getElementsByClassName("sd3")[j].innerHTML = playerArray[2].name;
+    document.getElementsByClassName("sd4")[j].innerHTML = playerArray[3].name;
+    document.getElementsByClassName("sd5")[j].innerHTML = playerArray[4].name;
+    document.getElementsByClassName("sd6")[j].innerHTML = playerArray[5].name;
+    document.getElementsByClassName("sd7")[j].innerHTML = playerArray[6].name;
+    document.getElementsByClassName("sd8")[j].innerHTML = playerArray[7].name;
+    }
+
+  } else {
+    for (j = 0; j<6; j++){
+    document.getElementsByClassName("sd1")[j].innerHTML = playerArray[0].name;
+    document.getElementsByClassName("sd2")[j].innerHTML = playerArray[1].name;
+    document.getElementsByClassName("sd3")[j].innerHTML = playerArray[2].name;
+    document.getElementsByClassName("sd4")[j].innerHTML = playerArray[3].name;
+    document.getElementsByClassName("sd5")[j].innerHTML = playerArray[4].name;
+    document.getElementsByClassName("sd6")[j].innerHTML = playerArray[5].name;
+    }
   }
+
 }
 
 // Called everytime a checkbox is clicked. Passes playerID for the 2 winners, the checkbox ID, and playerID for 2 losers.
@@ -68,6 +100,7 @@ function generate(){
         playerArray[l1].losses++;
         playerArray[l2].losses++;
         checkboxLoser.disabled = true;
+        totalWins = totalWins+2;
       //In case the user makes a mistake, they can uncheck the checkbox to change their option. Revert the scores
       }else{
         playerArray[w1].wins--;
@@ -75,6 +108,7 @@ function generate(){
         playerArray[l1].losses--;
         playerArray[l2].losses--;
         checkboxLoser.disabled = false;
+        totalWins = totalWins-2;
       }
 
       //Update the four player's points in scoreboard
@@ -84,10 +118,15 @@ function generate(){
       document.getElementById("tp"+(l2+1)+"score").innerHTML = playerArray[l2].wins+"-"+playerArray[l2].losses;
 
       //Update the four player's remaining games in scoreboard
-      document.getElementById("tp"+(w1+1)+"games").innerHTML = 6 - (playerArray[w1].wins+playerArray[w1].losses);
-      document.getElementById("tp"+(w2+1)+"games").innerHTML = 6 - (playerArray[w2].wins+playerArray[w2].losses);
-      document.getElementById("tp"+(l1+1)+"games").innerHTML = 6 - (playerArray[l1].wins+playerArray[l1].losses);
-      document.getElementById("tp"+(l2+1)+"games").innerHTML = 6 - (playerArray[l2].wins+playerArray[l2].losses);
+      document.getElementById("tp"+(w1+1)+"games").innerHTML = numOfGames - (playerArray[w1].wins+playerArray[w1].losses);
+      document.getElementById("tp"+(w2+1)+"games").innerHTML = numOfGames - (playerArray[w2].wins+playerArray[w2].losses);
+      document.getElementById("tp"+(l1+1)+"games").innerHTML = numOfGames - (playerArray[l1].wins+playerArray[l1].losses);
+      document.getElementById("tp"+(l2+1)+"games").innerHTML = numOfGames - (playerArray[l2].wins+playerArray[l2].losses);
+
+      document.getElementById("tp"+(w1+1)+"rng").innerHTML = (playerArray[w1].wins / totalWins * 100) + "%";
+      document.getElementById("tp"+(w2+1)+"rng").innerHTML = (playerArray[w2].wins / totalWins * 100)+ "%";
+      document.getElementById("tp"+(l1+1)+"rng").innerHTML = (playerArray[l1].wins / totalWins * 100)+ "%";
+      document.getElementById("tp"+(l2+1)+"rng").innerHTML = (playerArray[l2].wins / totalWins * 100)+ "%";
   }
 
 function displayRaffle(){
@@ -99,7 +138,6 @@ function closeRaffle(){
 }
 
 function selectRaffle(){
-  var totalWins = 18;
   var timer = 3;
   document.getElementsByClassName('pyro')[0].style.visibility = "hidden";
   for(let i = 0; i < playerArray.length; i++){
